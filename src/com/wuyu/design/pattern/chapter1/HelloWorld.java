f26f81b30af8eb66015d449f6e1aa06d2d22f1be
悟羽 <jinqing.djq@alibaba-inc.com>
2022-09-04 00:18:53 +0800

    feat: project init

---
+++ src/com/wuyu/design/pattern/chapter1/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.wuyu.design.pattern.chapter1;
+
+/**
+ * @author jinqing.djq 悟羽
+ * @date 2022/9/4 12:18 上午
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+    }
+}
